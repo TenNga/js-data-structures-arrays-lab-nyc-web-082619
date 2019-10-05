98318818e407f59a37c8048fd15c518411f5dcdb
Kevin Karma <kevinkarma55@gmail.com>
2019-10-05 18:00:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   drivers.push(name);
 }
+
+function destructivelyPrependDriver(name) {
+  drivers.unsplash(name);
+}
